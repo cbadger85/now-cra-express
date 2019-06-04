3e23638680f06cafaa299f6b551bfcd7ba72c45d
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2019-06-04 13:39:03 -0500

    connected api to react app

--- www/src/App.js
+++ www/src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [time, setTime] = useState('');
+
+  const handleSetTime = async () => {
+    const serverTime = await axios.get('/api/time');
+
+    setTime(serverTime.data);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Push the button</p>
+      <p className="time">{time}</p>
+      <button type="button" className="button" onClick={handleSetTime}>
+        Get the Time!
+      </button>
     </div>
   );
 }
